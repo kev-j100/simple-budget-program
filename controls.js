e2dfdd11595f718f063e99fdf42265b6d383def8
Kevin <kevj100@gmail.com>
2016-01-18 14:17:46 -0800

    add JQuery functions instead of fregular JavaScript to have easier popup dialog boxes

--- controls.js
+++ controls.js
@@ -1,64 +1,64 @@
-var accountForm = document.getElementById("new-account-form");
-var newAccount = document.getElementById("top-controls");
-var accountCancel = document.getElementById("accountCancel");
-var accountOk = document.getElementById("accountOk");
-var accountTable = document.getElementById("account-table");
+$(function(){
+	var dialog, accountForm,
+	accountName = $("#accountNameEntered"),
+	accountTotal = $("#accountTotalEntered"),
+	accountType = $("#accountTypeEntered"),
+	accountNotes = $("#accountNotesEntered"),
+	allAccountFields = $([]).add(accountName).add(accountTotal).add(accountType).add(accountNotes),
+	accountTable = $("#account-table");
 
-accountForm.style.display = "none";
+	var db = new Dexie("budget-database");
 
-//Defines database
-var db = new Dexie("budget-database");
+	db.version(1).stores({
+		accounts: '++id,name,total,type,notes'
+	});
 
-db.version(1).stores({
-	accounts: '++id,name,total,type,notes'
-});
+	//Open the database
+	db.open();
 
-//Open the database
-db.open();
+//Initial population of the accounts table
+	db.accounts.each(function (accounts){
+	addToTable(accounts.name,accounts.total);
+	});
 
-db.accounts.each(function (accounts){
-console.log(accounts.name);
-console.log(accounts.total);
-console.log(accounts.id);
-addToTable(accounts.name,accounts.total);
-});
-
-//console.log("I am running")
-
-//Shows the new account form
-function addAccount(){
-	accountForm.style.display = "";
-	}
-
-//Click the new account button to show
-newAccount.onclick = function(){
-addAccount();
-	};
-
-accountOk.onclick = function () {
-	db.accounts.add({name:document.getElementById("accountNameEntered").value,total:document.getElementById("accountTotalEntered").value,type:document.getElementById("accountTypeEntered").value,notes:document.getElementById("accountNotesEntered").value});
-	addToTable(document.getElementById("accountNameEntered").value,document.getElementById("accountTotalEntered").value)
-};
 
 //Adds to the table of accounts
-function addToTable(accountName,accountTotal){
-	//console.log(accountName);
-	//console.log(accountTotal);
-	var row = accountTable.insertRow();
-	var cell1 = row.insertCell();
-	var cell2 = row.insertCell();
-	cell1.innerHTML = accountName
-	cell2.innerHTML = accountTotal
+function addToTable(name,total){
+   accountTable.append("<tr>" + "<td>" + name + "</td>" + "<td>" + total + "</td>" + "</tr>");
 }
 
-//Cancels the new account form
-function hideAccountForm(){
-	accountForm.style.display = "none";
-	document.getElementById("accountNameEntered").value = ""
-	document.getElementById("accountTotalEntered").value = ""
-	document.getElementById("accountTypeEntered").value = ""
-	document.getElementById("accuntNotesEntered").value = ""
+//Add to the account
+function addAccount(){
+	db.accounts.add({name:accountName.val(),total:accountTotal.val(),type:accountType.val(),notes:accountNotes.val()});
+	addToTable(accountName.val(),accountTotal.val());
+	dialog.dialog("close");
+	return true;
 }
-accountCancel.onclick = function(){
-hideAccountForm();
-}
+
+//Dialog for account
+	dialog = $("#new-account-form").dialog({
+		autoOpen:false,
+		height: 300,
+		width:350,
+		modal: true,
+		buttons: {
+			"New Account": addAccount,
+		Cancel: function(){
+			dialog.dialog("close");
+		}
+	},
+		close:function(){
+			accountForm[0].reset();
+			allAccountFields.removeClass("ui-state-error");
+		}
+	});
+
+	accountForm	= dialog.find("form").on("submit",function(event){
+		event.preventDefault();
+		addAccount();
+	});
+
+	$("#top-controls").button().on("click",function(){
+		dialog.dialog("open");
+	});
+});
