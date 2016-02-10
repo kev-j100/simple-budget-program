dc1850a86348d8025eabd226438714e53275ca30
Kevin <kevj100@gmail.com>
2016-02-09 21:12:00 -0800

    Add dialog for credit

--- controls.js
+++ controls.js
@@ -30,6 +30,8 @@ function tableSetup(){
 	deleteAccount(accounts.id);
 	makeUpdateDialog(accounts.name,accounts.total,accounts.type,accounts.notes,accounts.id);
     updateAccount(accounts.id);
+	makeCreditDialog(accounts.id);
+    creditAccount(accounts.id);
 	});
 }
 
@@ -39,18 +41,21 @@ function refreshAccountTable(){
 	$(".infoDiv").remove();
 	$(".deleteAccountDialogDiv").remove();
 	$(".updateDialogContainDiv").remove();
+	$(".creditDiv").remove();
 	tableSetup();
 }
 
 //Adds to the table of accounts
 function addToTable(name,total,id){
    //accountTable.append("<tr class='accountTableRow'>" + "<td>" + name + "</td>" + "<td>" + total + "</td>" + "<td>" + "<ul id='menu_"+ id +"'><li>&vellip;<ul><li><div id='infoButton_"+ id +"'>Info</div>" + "</li><li id='editButton_"+ id +"'>Edit</li><li><div id='delete_"+ id +"'>Delete</div></li></ul></li></ul></td>" + "</tr>");
-   accountTable.append("<tr class='accountTableRow'>" + "<td>" + name + "</td>" + "<td>" + total + "</td>" + "<td>" + "<td><div id='menuButton"+ id +"' class='menuButton'>&vellip;</div><div id='infoButton_"+ id +"' class='menuItem'>Info</div><div id='editButton_"+ id +"' class='menuItem'>Edit</div><div id='delete_"+ id +"' class='menuItem'>Delete</div></td>" + "</tr>");
+   accountTable.append("<tr class='accountTableRow'>" + "<td>" + name + "</td>" + "<td>" + total + "</td>" + "<td>" + "<td><div id='menuButton"+ id +"' class='menuButton'>&vellip;</div><div id='infoButton_"+ id +"' class='menuItem'>Info</div><div id='editButton_"+ id +"' class='menuItem'>Edit</div><div id='delete_"+ id +"' class='menuItem'>Delete</div><div id='Credit"+ id +"' class='menuItem'>Credit</div><div id='Debit"+ id +"' class='menuItem'>Debit</div></td></tr>");
    $('.menuItem').button().hide();
    $('#menuButton'+id).button().click(function(){
 	   $('#infoButton_'+id).toggle("slow");
 	   $('#editButton_'+id).toggle("slow");
 	   $('#delete_'+id).toggle("slow");
+	   $('#Credit'+id).toggle("slow");
+	   $('#Debit'+id).toggle("slow");
    });
 }
 
@@ -66,7 +71,12 @@ function makeDeleteDialog(name,id){
 
 //Adds a dialog to update accounts
 function makeUpdateDialog(name,total,type,notes,id){
-	$("body").append("<div id='updateForm"+id+"' class='updateDialogContainDiv'><form id=update-form-form"+id+"'><fieldset><label>Name:</label><input type='text' id='accountNameEntered' name='accountNameEntered' value='"+name+"'/><label>Total:</label><input type='number' step='.01' id='accountTotalEntered' name='Total' value='0.00'/><label>Type:</label><input type='text' id='accountTypeEntered' name='accountTypeEntered'/><label>Notes:</label><textarea rows='4' cols='20' id='accountNotesEntered' name='accountNotesEntered'></textarea></fieldset></form></div>");
+	$("body").append("<div id='updateForm"+id+"' class='updateDialogContainDiv'><form id='update-form-form"+id+"'><fieldset><label>Name:</label><input type='text' id='accountNameEntered' name='accountNameEntered' value='"+name+"'/><label>Total:</label><input type='number' step='.01' id='accountTotalEntered' name='Total' value='0.00'/><label>Type:</label><input type='text' id='accountTypeEntered' name='accountTypeEntered'/><label>Notes:</label><textarea rows='4' cols='20' id='accountNotesEntered' name='accountNotesEntered'></textarea></fieldset></form></div>");
+}
+
+//Adds a dialog to the body for Debit
+function makeCreditDialog(id){
+	$("body").append("<div id='creditForm"+id+"' class='creditDiv'><h2>Credit</h2><form><label>Total:</label><input id='creditTotal"+ id +"' type='number' step='.01' value='0.00'/><label>Credit From:</label><input type='text' id='creditFrom"+ id +"'/><label>Notes:</label><textarea rows='4' cols='20' id='creditNotes"+ id +"'/><label>Date:</label><input type='date' id='creditDate"+ id +"'/></form></div>");
 }
 
 //Add to the account
@@ -144,6 +154,7 @@ function deleteAccount(id){
 	});
 }
 
+//Update Account functions
 function updateAccount(id){
 	$("#updateForm"+id).dialog({
 		autoOpen:false,
@@ -172,6 +183,36 @@ function updateAccount(id){
 
 	$("#editButton_"+id).on("click",function(){
 		$("#updateForm"+id).dialog("open");
+	});
+}
+
+function creditAccount(id){
+	$("#creditForm"+id).dialog({
+		autoOpen:false,
+		height: 300,
+		width:350,
+		modal: true,
+		buttons: {
+			"Add Credit": function(){
+				$(this).dialog("close");
+				console.log($("#updateForm"+id+" #accountNameEntered").val());
+				console.log($("#updateForm"+id+" #accountTotalEntered").val());
+				console.log($("#updateForm"+id+" #accountTypeEntered").val());
+				console.log($("#updateForm"+id+" #accountNotesEntered").val());
+			    refreshAccountTable();
+			},
+		Cancel: function(){
+			$("#creditForm"+id).dialog("close");
+		}
+	},
+		close:function(){
+			accountForm[0].reset();
+			allAccountFields.removeClass("ui-state-error");
+		}
+	});
+
+	$("#Credit"+id).on("click",function(){
+		$("#creditForm"+id).dialog("open");
 	});
 }
 });
