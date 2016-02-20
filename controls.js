3c5de3b29bf004633448099b58137894d1a692ed
Kevin <kevj100@gmail.com>
2016-02-19 18:28:53 -0800

    Add split dollar and cent method/ Add credit and debit functions

--- controls.js
+++ controls.js
@@ -1,11 +1,14 @@
 $(function(){
 	var dialog, accountForm,
+	allAccountsTotal = 0,
 	accountName = $("#accountNameEntered"),
-	accountTotal = $("#accountTotalEntered"),
+	accountTotalDollars = $("#accountTotalDollarsEntered"),
+	accountTotalCents = $("#accountTotalCentsEntered"),
 	accountType = $("#accountTypeEntered"),
 	accountNotes = $("#accountNotesEntered"),
-	allAccountFields = $([]).add(accountName).add(accountTotal).add(accountType).add(accountNotes),
-	accountTable = $("#account-table");
+	allAccountFields = $([]).add(accountName).add(accountTotalDollars).add(accountTotalCents).add(accountType).add(accountNotes),
+	accountTable = $("#account-table"),
+	currencyStmbol = "$";
 
 	var db = new Dexie("budget-database");
 
@@ -20,9 +23,11 @@ $(function(){
 //Initial population of the accounts table
 tableSetup();
 
+
 //Adds all account items to table
 function tableSetup(){
 	db.accounts.each(function (accounts){
+
 	addToTable(accounts.name,accounts.total,accounts.id);
 	addInfoToBody(accounts.name,accounts.total,accounts.type,accounts.notes,accounts.id);
 	makeInfoDialog(accounts.id);
@@ -38,6 +43,10 @@ function tableSetup(){
 	});
 }
 
+function updateAccountsTotal(total){
+	allAccountsTotal = allAcountsTotal + total;
+}
+
 //Refresh the table
 function refreshAccountTable(){
 	$(".accountTableRow").remove();
@@ -51,8 +60,10 @@ function refreshAccountTable(){
 
 //Adds to the table of accounts
 function addToTable(name,total,id){
-   //accountTable.append("<tr class='accountTableRow'>" + "<td>" + name + "</td>" + "<td>" + total + "</td>" + "<td>" + "<ul id='menu_"+ id +"'><li>&vellip;<ul><li><div id='infoButton_"+ id +"'>Info</div>" + "</li><li id='editButton_"+ id +"'>Edit</li><li><div id='delete_"+ id +"'>Delete</div></li></ul></li></ul></td>" + "</tr>");
-   accountTable.append("<tr class='accountTableRow'>" + "<td>" + name + "</td>" + "<td>" + total + "</td>" + "<td>" + "<td><div id='menuButton"+ id +"' class='menuButton'>&vellip;</div><div id='infoButton_"+ id +"' class='menuItem'>Info</div><div id='editButton_"+ id +"' class='menuItem'>Edit</div><div id='delete_"+ id +"' class='menuItem'>Delete</div><div id='Credit"+ id +"' class='menuItem'>Credit</div><div id='Debit"+ id +"' class='menuItem'>Debit</div></td></tr>");
+
+	var moneyDisplay = total.toString().slice(0,-2) + "." + total.toString().slice(-2);
+
+    accountTable.append("<tr class='accountTableRow'>" + "<td>" + name + "</td>" + "<td>"+ currencyStmbol + moneyDisplay + "</td>" + "<td>" + "<td><div id='menuButton"+ id +"' class='menuButton'>&vellip;</div><div id='infoButton_"+ id +"' class='menuItem'>Info</div><div id='editButton_"+ id +"' class='menuItem'>Edit</div><div id='delete_"+ id +"' class='menuItem'>Delete</div><div id='Credit"+ id +"' class='menuItem'>Credit</div><div id='Debit"+ id +"' class='menuItem'>Debit</div></td></tr>");
    $('.menuItem').button().hide();
    $('#menuButton'+id).button().click(function(){
 	   $('#infoButton_'+id).toggle("slow");
@@ -65,7 +76,10 @@ function addToTable(name,total,id){
 
 //Add div to the body for info dialog
 function addInfoToBody(name,total,type,notes,id){
-	$("body").append("<div id='info_"+ id +"' class='infoDiv'>Name: "+ name +"<br>Total: "+ total +"<br>Type:" +type+ "<br>Notes: "+ notes +"</div>");
+
+	var moneyDisplay = total.toString().slice(0,-2) + "." + total.toString().slice(-2);
+
+	$("body").append("<div id='info_"+ id +"' class='infoDiv'>Name: "+ name +"<br>Total: "+ currencyStmbol + moneyDisplay +"<br>Type: "+ type + "<br>Notes: "+ notes +"</div>");
 }
 
 //Adds in Delete Dialog for each account
@@ -75,23 +89,38 @@ function makeDeleteDialog(name,id){
 
 //Adds a dialog to update accounts
 function makeUpdateDialog(name,total,type,notes,id){
-	//console.log(notes);
-	$("body").append("<div id='updateForm"+id+"' class='updateDialogContainDiv'><form id='update-form-form"+id+"'><fieldset><label>Name:</label><input type='text' id='accountNameEntered' name='accountNameEntered' value='"+name+"'/><label>Total:</label><input type='number' step='.01' id='accountTotalEntered' name='Total' value='"+total+"'/><label>Type:</label><input type='text' id='accountTypeEntered' name='accountTypeEntered' value='"+type+"'/><label>Notes:</label><textarea rows='4' cols='20' id='accountNotesEntered' name='accountNotesEntered' value='"+notes+"'>"+notes+"</textarea></fieldset></form></div>");
+
+	var moneyDisplay1 = total.toString().slice(0,-2);
+ 	var moneyDisplay2 = total.toString().slice(-2);
+
+	$("body").append("<div id='updateForm"+id+"' class='updateDialogContainDiv'><form id='update-form-form"+id+"'><fieldset><label>Name:</label><input type='text' id='accountNameEntered' name='accountNameEntered' value='"+name+"'/><label>Total:</label>"+currencyStmbol+"<input type='number' step='1' id='accountTotalDollarsEntered' name='Total' value='"+moneyDisplay1+"' min='0'/>.<input type='number' step='1' id='accountTotalCentsEntered' name='Total' value='"+moneyDisplay2+"' min='0' max='99'/><label>Type:</label><input type='text' id='accountTypeEntered' name='accountTypeEntered' value='"+type+"'/><label>Notes:</label><textarea rows='4' cols='20' id='accountNotesEntered' name='accountNotesEntered' value='"+notes+"'>"+notes+"</textarea></fieldset></form></div>");
 }
 
 //Adds a dialog to the body for Credit
 function makeCreditDialog(id){
-	$("body").append("<div id='creditForm"+id+"' class='creditDiv'><h2>Credit</h2><form id='credit-form-form'><label>Total:</label><input id='creditTotalEntered' type='number' step='.01' value='0.00'/><label>Credit From:</label><input type='text' id='creditFromEntered'/><label>Notes:</label><textarea rows='4' cols='20' id='creditNotesEntered'/><label>Date:</label><input type='date' id='creditDateEntered'/></form></div>");
+	$("body").append("<div id='creditForm"+id+"' class='creditDiv'><h2>Credit</h2><form id='credit-form-form'><label>Total:</label>"+currencyStmbol+"<input id='creditTotalDollarsEntered' type='number' step='1' value='0' min='0'/>.<input id='creditTotalCentsEntered' type='number' step='1' value='0' min='0' max='99'/><label>Credit From:</label><input type='text' id='creditFromEntered'/><label>Notes:</label><textarea rows='4' cols='20' id='creditNotesEntered'/><label>Date:</label><input type='date' id='creditDateEntered'/></form></div>");
 }
 
 //Adds a dialog to the body for Debit
 function makeDebitDialog(id){
-	$("body").append("<div id='debitForm"+id+"' class='debitDiv'><h2>Debit</h2><form id='debit-form-form'><label>Total:</label><input id='debitTotalEntered' type='number' step='.01' value='0.00'/><label>Debit To:</label><input type='text' id='debitFromEntered'/><label>Notes:</label><textarea rows='4' cols='20' id='debitNotesEntered'/><label>Date:</label><input type='date' id='debitDateEntered'/></form></div>");
+	$("body").append("<div id='debitForm"+id+"' class='debitDiv'><h2>Debit</h2><form id='debit-form-form'><label>Total:</label>"+currencyStmbol+"<input id='debitTotalDollarsEntered' type='number' step='1' value='0' min='0'/>.<input id='debitTotalCentsEntered' type='number' step='1' value='0' min='0' max='99'/><label>Debit To:</label><input type='text' id='debitFromEntered'/><label>Notes:</label><textarea rows='4' cols='20' id='debitNotesEntered'/><label>Date:</label><input type='date' id='debitDateEntered'/></form></div>");
 }
 
 //Add to the account
 function addAccount(){
-	db.accounts.add({name:accountName.val(),total:accountTotal.val(),type:accountType.val(),notes:accountNotes.val()});
+	var accountTotal = "0"
+	var dollars = accountTotalDollars.val();
+	var cents = accountTotalCents.val();
+
+	if (cents.length == 1){
+		var revAccountTotalCents = "0" + cents;
+		accountTotal = dollars.toString() + revAccountTotalCents.toString();
+	}
+	else{
+		accountTotal = dollars.toString() + cents.toString();
+	}
+
+	db.accounts.add({name:accountName.val(),total:accountTotal,type:accountType.val(),notes:accountNotes.val()});
 	refreshAccountTable();
 	dialog.dialog("close");
 	return true;
@@ -174,9 +203,21 @@ function updateAccount(id){
 		buttons: {
 			"Update Account": function(){
 				$(this).dialog("close");
-				console.log($("#updateForm"+id+" #accountNameEntered").val())
+
+					var accountTotal = "0"
+	                var dollars = $("#updateForm"+id+" #accountTotalDollarsEntered").val();
+	                var cents = $("#updateForm"+id+" #accountTotalCentsEntered").val();
+
+	                if (cents.length == 1){
+		                   var revAccountTotalCents = "0" + cents;
+		                   accountTotal = dollars.toString() + revAccountTotalCents.toString();
+	                    }
+	                else{
+		                   accountTotal = dollars.toString() + cents.toString();
+	                    }
+
 				db.accounts.update(id,{name: $("#updateForm"+id+" #accountNameEntered").val()});
-				db.accounts.update(id,{total: $("#updateForm"+id+" #accountTotalEntered").val()});
+				db.accounts.update(id,{total: accountTotal});
 				db.accounts.update(id,{type: $("#updateForm"+id+" #accountTypeEntered").val()});
 				db.accounts.update(id,{notes: $("#updateForm"+id+" #accountNotesEntered").val()});
 			    refreshAccountTable();
@@ -206,13 +247,34 @@ function creditAccount(id){
 		buttons: {
 			"Add Credit": function(){
 				$(this).dialog("close");
-            var creditTotal = $("#creditForm"+id+" #creditTotalEntered").val();
+
+            var dollars = $("#creditForm"+id+" #creditTotalDollarsEntered").val();
+			var cents = $("#creditForm"+id+" #creditTotalCentsEntered").val();
 			var creditFrom = $("#creditFrom"+id+" #creditFromEntered").val();
 			var creditNotes = $("#creditFrom"+id+" #creditNotesEntered").val();
 			var creditDate = $("#creditFrom"+id+" #creditDateEntered").val();
 
-				db.transaction('rw',db.accounts,db.credits,function(){
-				   db.credits.add({total:creditTotal,credit_from:creditFrom,notes:creditNotes,date:creditDate,accountKey:id});
+				var creditTotal = "0"
+
+	            if (cents.length == 1){
+		                var revAccountTotalCents = "0" + cents;
+		                creditTotal = dollars.toString() + revAccountTotalCents.toString();
+	                }
+	            else{
+		                creditTotal = dollars.toString() + cents.toString();
+	                }
+
+			    db.credits.add({total:creditTotal,credit_from:creditFrom,notes:creditNotes,date:creditDate,accountKey:id});
+
+				db.transaction('rw',db.accounts,function(){
+				db.accounts.get(id).then(function(account){
+
+		          var newTotal = parseInt(creditTotal) + parseInt(account.total);
+
+				  db.accounts.update(id,{total: newTotal.toString()});
+
+	         });
+
 				}).catch(function(error){
 					alert("Ooops: " + error);
 				});
@@ -244,13 +306,33 @@ function debitAccount(id){
 		buttons: {
 			"Add Debit": function(){
 				$(this).dialog("close");
-            var debitTotal = $("#debitForm"+id+" #debitTotalEntered").val();
-			var debitTo = $("#debitFrom"+id+" #debitFromEntered").val();
-			var debitNotes = $("#debitFrom"+id+" #debitNotesEntered").val();
-			var debitDate = $("#debitFrom"+id+" #debitDateEntered").val();
+             var dollars = $("#debitForm"+id+" #debitTotalDollarsEntered").val();
+			var cents = $("#debitForm"+id+" #debitTotalCentsEntered").val();
+			var debitFrom = $("#debitFrom"+id+" #creditFromEntered").val();
+			var debitNotes = $("#debitFrom"+id+" #creditNotesEntered").val();
+			var debitDate = $("#debitFrom"+id+" #creditDateEntered").val();
 
-				db.transaction('rw',db.accounts,db.credits,function(){
-				   db.debits.add({total:debitTotal,debit_to:debitTo,notes:debitNotes,date:debitDate,accountKey:id});
+				var debitTotal = "0"
+
+	            if (cents.length == 1){
+		                var revAccountTotalCents = "0" + cents;
+		                debitTotal = dollars.toString() + revAccountTotalCents.toString();
+	                }
+	            else{
+		                debitTotal = dollars.toString() + cents.toString();
+	                }
+
+			    db.debits.add({total:debitTotal,credit_from:debitFrom,notes:debitNotes,date:debitDate,accountKey:id});
+
+				db.transaction('rw',db.accounts,function(){
+				db.accounts.get(id).then(function(account){
+
+		          var newTotal =  parseInt(account.total) - parseInt(debitTotal);
+
+				  db.accounts.update(id,{total: newTotal.toString()});
+
+	         });
+
 				}).catch(function(error){
 					alert("Ooops: " + error);
 				});
@@ -268,8 +350,17 @@ function debitAccount(id){
 	});
 
 	$("#Debit"+id).on("click",function(){
-		console.log("clicked");
 		$("#debitForm"+id).dialog("open");
 	});
 }
+
+ $( ".spinner").spinner({
+      min: 5,
+      max: 2500,
+      step: .01,
+      start: 1000,
+      numberFormat: "c",
+	  culture: "en-US"
+    });
+
 });
